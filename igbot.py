97cc1297591a7833c4b4cf0db70547a097314df1
geeseanonymous <66060314+geeseanonymous@users.noreply.github.com>
2020-10-17 15:22:49 +0800

    Update igbot.py

--- igbot.py
+++ igbot.py
@@ -6,7 +6,7 @@ bot = instabot.Bot()
 
 bot.login(username="turtles_every_hour",password="Turtles_All_The_Time@102938")
 
-path = 'C:/Users/tzeka/PycharmProjects'
+path = '/home/geeseanonymous/.virtualenvs/myvirtualenv/igbot/pics'
 for image in path:
     bot.upload_photo(image,caption="nice hour for turtles")
     sleep(7200)
